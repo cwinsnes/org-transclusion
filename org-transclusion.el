5d660939f0c7248fffdc54e1bfb0b0c03c520d79
Casper Winsnes <cwinsnes92@gmail.com>
2020-08-16 17:28:00 +0200

    Add start and end arguments to transclusion.

--- org-transclusion.el
+++ org-transclusion.el
@@ -13,8 +13,12 @@
   :group 'org
   :prefix 'org-transclusion-)
 
-(defcustom org-transclusion-regex "{{Transclude: \\(.+?\\)}}"
-  "String used to identify Transclusion areas."
+(defcustom org-transclusion-regex
+  "{{Transclude: \\(.+?\\)\\([[:blank:]]+[[:digit:]]+\\)?\\([[:blank:]]+[[:digit:]]+\\)?}}"
+  "String used to identify Transclusion areas.
+The regex should include at least 1 group (\\1), naming the file to be
+transcluded and 2 optional groups (\\2 and \\2) that inform which lines
+to be included from the file."
   :group 'org-transclusion
   :type 'string)
 
@@ -23,19 +27,31 @@
   (setq org-transclusion-mode-map (make-sparse-keymap))
   (define-key org-transclusion-mode-map (kbd "C-M-x") 'org-transclusion-find-transclusions))
 
-(defun org-transclusion-read-file (path)
-  "Read the file at PATH and return its contents as a string."
+(defun org-transclusion-read-file (path &optional start end)
+  "Read the file at PATH and return its contents as a string.
+Optionally only returns the lines between START and END."
   (with-temp-buffer
     (insert-file-contents path)
+    (when end
+      (end-of-line end)
+      (let ((current-point (point)))
+        (kill-region current-point (point-max))))
+    (when (and start (> start 0))
+      (goto-line 0)
+      (kill-whole-line (- start 1)))
     (buffer-string)))
 
-(defun org-transclusion-transclude-file (filename transclusion-point)
+(defun org-transclusion-transclude-file (filename transclusion-point &optional start end)
   "Insert the transclusion of FILENAME at TRANSCLUSION-POINT.
-This will add an overlay to the transcluded text to keep the text read-only."
+Can optionally force the transclusion to only happen from the
+lines START to END.
+If only START is supplied, go to the end of the file from START.
+This will add an overlay to the transcluded
+text to keep the text read-only."
   (save-excursion
     (goto-char transclusion-point)
     (insert "\n") ;; Make sure a new line exists after transclusion regex text
-    (let* ((file-string (org-transclusion-read-file filename))
+    (let* ((file-string (org-transclusion-read-file filename start end))
            (overlay (ov-insert file-string)))
       (ov-read-only overlay t nil)
       (ov-set overlay 'face 'font-lock-warning-face)
@@ -50,15 +66,23 @@ This will add an overlay to the transcluded text to keep the text read-only."
       (while (re-search-forward org-transclusion-regex nil t)
         (when (match-string 0)
           (let ((transclusion-point (match-end 0))
-                (filename (match-substitute-replacement "\\1")))
-            (org-transclusion-transclude-file filename transclusion-point)
+                (filename (match-substitute-replacement "\\1"))
+                (start-point (match-substitute-replacement "\\2"))
+                (end-point (match-substitute-replacement "\\3")))
+
+            (if (equal start-point "")
+              (setq start-point nil)
+              (setq start-point (string-to-number start-point)))
+            (if (equal end-point "")
+              (setq end-point nil)
+              (setq end-point (string-to-number end-point)))
+            (org-transclusion-transclude-file filename transclusion-point start-point end-point)
             ))))))
 
 (define-minor-mode org-transclusion-mode
   "Toggle org-transclusion-mode.
 When org-transclusion-mode is enabled, add transclusion functionality to org-mode using
 the syntax from the \\[org-transclusion-regex] variable."
-
   :init-value nil
   :lighter "org-tcx"
   :group 'org-transclusion
